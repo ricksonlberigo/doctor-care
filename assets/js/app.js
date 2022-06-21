5d7e33e7ef26cff2b33adf0fe1d22e5bae8dcc2a
RicksonLBerigo <ricksonl.berigo@outlook.com>
2022-06-21 12:18:25 -0300

    Fixing bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,56 +3,71 @@ window.addEventListener('scroll', onScroll)
 onScroll()
 function onScroll() {
   scrollNavBackground()
-  showBackToTopButton()
+  backToTopButtonShow()
 
-  activateMenuAtCurrentSection(home)
-  activateMenuAtCurrentSection(services)
-  activateMenuAtCurrentSection(about)
-  activateMenuAtCurrentSection(testimonials)
-  activateMenuAtCurrentSection(contact)
+  activeMenuCurrentSection()
 }
 
 function scrollNavBackground() {
+  const nav = document.querySelector('#navigation')
   if (scrollY > 0) {
-    navigation.classList.add('scroll')
+    nav.classList.add('scroll')
   } else {
-    navigation.classList.remove('scroll')
+    nav.classList.remove('scroll')
   }
 }
 
-function showBackToTopButton() {
-  if (scrollY >= 550) {
-    backToTop.classList.add('show')
+function backToTopButtonShow() {
+  const backToTopButton = document.getElementById('backToTop')
+  if (scrollY >= 560) {
+    backToTopButton.classList.add('show')
   } else {
-    backToTop.classList.remove('show')
+    backToTopButton.classList.remove('show')
   }
 }
 
+function activeMenuCurrentSection() {
+  const sections = document.querySelectorAll('main section[id]')
+  const checkpoints = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoints >= sectionTop
+    const checkpointEnd = checkpoints <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('.nav__item a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('.nav__item a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/* Não funciona no chrome
 function activateMenuAtCurrentSection(section) {
-  // Linha alvo
   const targetLine = scrollY + innerHeight / 2
 
-  // Topo da seção
   const sectionTop = section.offsetTop
-  // Tamanho da seção
+
   const sectionHeight = section.offsetHeight
 
-  // verificando se a linha alvo da seção chegou ao topo ou passou
   const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
 
-  // Final da seção
   const sectionEndsAt = sectionTop + sectionHeight
 
-  // Verificando se a linha da seção chegou ao seu fim
   const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
 
-  // Limites da seção onde elá deve passar do topo mas não pode passar do final
   const sectionsBoundaries =
     sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
 
-  // Pegando os ID's das seções
   const sectionId = section.getAttribute('id')
-  // Pegando os links do menu
   const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
 
   menuElement.classList.remove('active')
@@ -60,6 +75,7 @@ function activateMenuAtCurrentSection(section) {
     menuElement.classList.add('active')
   }
 }
+*/
 
 function openMenu() {
   document.body.classList.add('menu-expanded')
@@ -78,8 +94,8 @@ ScrollReveal({
 #home .header__section, #home .content, #home .stats, #home .stats .stat,
 #services .header__section, #services .service,
 #about .header__section, #about .content,
-#testimonials .header__section, #testimonials .content,
-#contact .header__section, #contact .content,
+#testimonials .header__section,
+#contact .header__section, #contact .col-b,
 #footer .footer__brand, #footer .social__list
 `)
 
