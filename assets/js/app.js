266fee358879c2709c594a32a03e244c2a468852
RicksonLBerigo <ricksonl.berigo@outlook.com>
2022-06-21 09:59:50 -0300

    Finishing the doctor care page

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,12 @@ onScroll()
 function onScroll() {
   scrollNavBackground()
   showBackToTopButton()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(testimonials)
+  activateMenuAtCurrentSection(contact)
 }
 
 function scrollNavBackground() {
@@ -22,6 +28,39 @@ function showBackToTopButton() {
   }
 }
 
+function activateMenuAtCurrentSection(section) {
+  // Linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // Topo da seção
+  const sectionTop = section.offsetTop
+  // Tamanho da seção
+  const sectionHeight = section.offsetHeight
+
+  // verificando se a linha alvo da seção chegou ao topo ou passou
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  // Final da seção
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // Verificando se a linha da seção chegou ao seu fim
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // Limites da seção onde elá deve passar do topo mas não pode passar do final
+  const sectionsBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  // Pegando os ID's das seções
+  const sectionId = section.getAttribute('id')
+  // Pegando os links do menu
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionsBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
 function openMenu() {
   document.body.classList.add('menu-expanded')
 }
@@ -29,6 +68,20 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+// Scroll reveal js
+ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  reset: true,
+}).reveal(`
+#home .header__section, #home .content, #home .stats, #home .stats .stat,
+#services .header__section, #services .service,
+#about .header__section, #about .content,
+#testimonials .header__section, #testimonials .content,
+#contact .header__section, #contact .content,
+#footer .footer__brand, #footer .social__list
+`)
 
 // Swiper js
 const swiper = new Swiper('.swiper', {
@@ -50,16 +103,3 @@ const swiper = new Swiper('.swiper', {
     },
   },
 })
-
-// Scroll reveal js
-ScrollReveal({
-  origin: 'top',
-  distance: '30px',
-  reset: true,
-}).reveal(`
-#home .header__section, #home .content, #home .stats, #home .stats .stat,
-#services .header__section, #services .service,
-#about .header__section, #about .content,
-#contact .header__section, #contact .content,
-#footer .footer__brand, #footer .social__list
-`)
